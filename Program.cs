b63f806625f5ec919bd88a4024bd0904354bdecf
Gabriel Palheiro <108705306+GabrielPalheiro@users.noreply.github.com>
2026-03-02 22:39:14 -0300

    update program.cs

--- Program.cs
+++ Program.cs
@@ -1,12 +1,32 @@
 using Microsoft.EntityFrameworkCore;
 using AgroSolutions_IngestionService.Data;
 
-var builder = Host.CreateApplicationBuilder(args);
+// Trocamos Host por WebApplication para habilitar o servidor HTTP (Kestrel)
+var builder = WebApplication.CreateBuilder(args);
 
+// 1. Configuração do Banco de Dados
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+// 2. Adiciona os Controllers (Essencial para o Postman funcionar)
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// 3. Mantém o seu Worker do RabbitMQ rodando em segundo plano
 builder.Services.AddHostedService<RabbitMQWorker>();
 
-var host = builder.Build();
-host.Run();
+var app = builder.Build();
+
+// 4. Configura o pipeline de requisições HTTP
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+app.MapControllers();
+
+// 5. Força a porta 8080 que o Kubernetes espera
+app.Run("http://*:8080");
